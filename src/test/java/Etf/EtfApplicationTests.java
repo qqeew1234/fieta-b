e2c27d425837b1731e82eb5a89942356779ebe99
chumin-yeong <chu35my@gmail.com>
2025-04-24 09:37:00 +0900

    initial commit

---
+++ src/test/java/Etf/EtfApplicationTests.java
@@ -0,0 +1,13 @@
+package Etf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
